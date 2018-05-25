0f9e2f4b2bf3a993064e995984a04ba412bdb1b6
Wei <gongwei619@hotmail.com>
2018-05-24 20:45:16 -0400

    Add a callback to report downloading progress

--- utils/download.py
+++ utils/download.py
@@ -1,8 +1,29 @@
 import os
+import sys
 from urllib.request import urlretrieve
 
 
 from config import DATA_ROOT, MNIST_URL
+
+
+last_reported_percentage = 0
+
+
+def report_progress(count_blocks, block_size, total_size):
+    """A call back to report the progress of downloads"""
+
+    global last_reported_percentage
+    percentage = int(100 * count_blocks * block_size / total_size)
+
+    if percentage != last_reported_percentage:
+        if percentage % 5 == 0:
+            sys.stdout.write('{}%'.format(percentage))
+            sys.stdout.flush()
+        else:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+
+    last_reported_percentage = percentage
 
 
 def maybe_download(filename, expected_bytes,
@@ -16,7 +37,7 @@ def maybe_download(filename, expected_bytes,
     if not os.path.exists(dest_filename) or force:
         print("\nAttempting to download:", filename)
         filename, _ = urlretrieve(url + filename, dest_filename,
-                                  reporthook=None)
+                                  reporthook=report_progress)
         print("\nDownload completed!")
 
     statinfo = os.stat(dest_filename)
