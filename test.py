571b402aeaaa6f5f7c179d71b6fd8f68ed4e2ceb
Wei <gongwei619@hotmail.com>
2018-05-24 21:34:26 -0400

    Add a new unit test for the util function for extracting images from downloaded files

--- test.py
+++ test.py
@@ -2,20 +2,33 @@ import os
 import unittest
 
 
-from config import DATA_ROOT
+from config import DATA_ROOT, NUM_CLASSES
 from utils.download import maybe_download
+from utils.extract import maybe_extract
 
 
 class UtilsCase(unittest.TestCase):
     """Unit tests for utilities"""
 
+    def setUp(self):
+        """Set up space for testing"""
+
+        self.filename = 'notMNIST_small.tar.gz'
+        self.num_classes = NUM_CLASSES
+
     def test_download(self):
         """Test the download function"""
 
-        filename = 'notMNIST_small.tar.gz'
-        test_filename = maybe_download('notMNIST_small.tar.gz', 8458043,
-                                       force=True)
-        self.assertEqual(test_filename, os.path.join(DATA_ROOT, filename))
+        test_filename = maybe_download(self.filename, 8458043, force=True)
+        self.assertEqual(test_filename, os.path.join(DATA_ROOT, self.filename))
+
+    def test_extract(self):
+        """Test downloaded file extraction"""
+
+        test_filename = os.path.join(DATA_ROOT, self.filename)
+        test_data_folders = maybe_extract(test_filename, force=True)
+        self.assertTrue(isinstance(test_data_folders, list))
+        self.assertEqual(len(test_data_folders), self.num_classes)
 
 
 if __name__ == '__main__':
