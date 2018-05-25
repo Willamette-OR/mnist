46207f492bcb76ebb566d8555f0e314a9d6d879a
Wei <gongwei619@hotmail.com>
2018-05-24 21:32:40 -0400

    Add a configuration variable for number of classes of mnist images

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 MNIST_URL = 'https://commondatastorage.googleapis.com/books1000/'
 DATA_ROOT = os.path.join(BASE_DIR, 'data')
+NUM_CLASSES = 10
